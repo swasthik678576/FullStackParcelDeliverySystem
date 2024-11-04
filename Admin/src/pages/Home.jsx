fc7ab50692cd9331f799dacc35af4553280fbc4d
Your Name <you@example.com>
2024-11-04 15:58:00 +0530

    finished the project

--- Admin/src/pages/Home.jsx
+++ Admin/src/pages/Home.jsx
@@ -1,7 +1,40 @@
 import { HiArrowSmallUp, HiArrowLongDown } from "react-icons/hi2";
 import { PieChart } from "@mui/x-charts/PieChart";
+import { useEffect, useState } from "react";
+import { publicRequest } from "../requestMethods";
 
 const Home = () => {
+  
+  const [parcels, setParcels] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  const usersCount = users.length;
+  const deliveredCount = parcels.filter(parcels => parcels.status === 3).length;
+  const pendingCount = parcels.filter(parcels => parcels.status === 1).length;
+
+  useEffect(() => {
+    const getParcels = async () => {
+      try {
+        const res = await publicRequest.get("/parcels");
+        setParcels(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getParcels();
+  }, []);
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const res = await publicRequest.get("/Users");
+        setUsers(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getUsers();
+  }, []);
+
   return (
     <div>
       <div className="flex items-center">
@@ -12,7 +45,7 @@ const Home = () => {
               <HiArrowSmallUp className="text-[28px] text-green-500" />
               <HiArrowLongDown className="text-[28px] text-red-500" />
             </div>
-            <span className="mt-[20px] text-[18px]">200</span>
+            <span className="mt-[20px] text-[18px]">{usersCount}</span>
           </div>
         </div>
         <div className="flex flex-col text-[#d9d9d9] h-[250px] w-[350px] shadow-lg m-[20px]">
@@ -22,7 +55,7 @@ const Home = () => {
               <HiArrowSmallUp className="text-[28px] text-green-500" />
               <HiArrowLongDown className="text-[28px] text-red-500" />
             </div>
-            <span className="mt-[20px] text-[18px]">2000</span>
+            <span className="mt-[20px] text-[18px]">{deliveredCount}</span>
           </div>
         </div>
         <div className="flex flex-col text-[#d9d9d9] h-[250px] w-[350px] shadow-lg m-[20px]">
@@ -32,34 +65,34 @@ const Home = () => {
               <HiArrowSmallUp className="text-[28px] text-green-500" />
               <HiArrowLongDown className="text-[28px] text-red-500" />
             </div>
-            <span className="mt-[20px] text-[18px]">100</span>
+            <span className="mt-[20px] text-[18px]">{pendingCount}</span>
           </div>
         </div>
       </div>
 
       <div className="flex items-center justify-between">
         <div className="h-[450px] w-[500px] text-[#fff]">
-        <PieChart
-          series={[
-            {
-              data: [
-                { id: 0, value: 10, label: "Pending Parcels" },
-                { id: 1, value: 15, label: "Delivered Parcels" },
-                { id: 2, value: 2, label: "Rejected Parcels" },
-              ],
-              innerRadius: 30,
-              outerRadius: 100,
-              paddingAngle: 5,
-              cornerRadius: 5,
-              startAngle: -45,
-              endAngle: 225,
-              cx: 150,
-              cy: 150,
-            },
-          ]}
-        />
+          <PieChart
+            series={[
+              {
+                data: [
+                  { id: 0, value: pendingCount, label: "Pending Parcels" },
+                  { id: 1, value: deliveredCount, label: "Delivered Parcels" },
+                  { id: 2, value: usersCount, label: "Users" },
+                ],
+                innerRadius: 30,
+                outerRadius: 100,
+                paddingAngle: 5,
+                cornerRadius: 5,
+                startAngle: -45,
+                endAngle: 225,
+                cx: 150,
+                cy: 150,
+              },
+            ]}
+          />
         </div>
-        
+
         <div className="h-[350px] w-[300px] shadow-lg p-[20px]">
           <h2 className="flex px-[20px] text-[#fff]">Recent Users</h2>
           <ol className="flex font-semibold flex-col justify-end px-[20px] mt-[10px] text-[#d9d9d9]">
@@ -67,7 +100,6 @@ const Home = () => {
             <li>2. Jane Doe</li>
             <li>3. Alex Doe</li>
           </ol>
-
         </div>
       </div>
     </div>
